44b4311255bb6340975a137262d02d4fc4f388fc
Edson Nakamura <enakamura3@gmail.com>
2019-01-07 01:18:06 -0200

    Initial commit

---
+++ src/main/java/org/naka/demo/springWebhook/SpringWebhookApplication.java
@@ -0,0 +1,14 @@
+package org.naka.demo.springWebhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebhookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebhookApplication.class, args);
+	}
+
+}
+
