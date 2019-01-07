8439808c16b1087000f3f08fd5b14c8d18ed78c4
Edson Nakamura <enakamura3@gmail.com>
2019-01-07 01:45:50 -0200

    Add properties files

--- src/main/java/org/naka/demo/springWebhook/SpringWebhookApplication.java
+++ src/main/java/org/naka/demo/springWebhook/SpringWebhookApplication.java
@@ -2,8 +2,10 @@ package org.naka.demo.springWebhook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringWebhookApplication {
 
 	public static void main(String[] args) {
